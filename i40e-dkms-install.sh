d970a18c95e74cb98f3aa2147e927ea85c4a1861
Chris Wedgwood <cw@f00f.org>
2018-06-21 23:05:51 +0000

    i40e-dkms-install.sh: remove debug option, make version somewhat agnostic

--- i40e-dkms-install.sh
+++ i40e-dkms-install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# XXX/TODO; install -headers pkgs for all kernels
+
 # eh :(
 set -e
 
 # defaults
 DRVURL="https://sourceforge.net/projects/e1000/files/i40e%20stable/2.4.6/i40e-2.4.6.tar.gz"
-DEBUG=0
 PREP=0
 TEMPDIR=1
 
@@ -14,7 +15,7 @@ ERR=0
 
 function usage() {
 cat <<EOF >&2
-Usage: $(basename $0) [-h] [-u driver-url] [-p http://proxy.to.use:port] [ -T ] [ -d ]
+Usage: $(basename $0) [-h] [-u driver-url] [-p http://proxy.to.use:port] [ -T ]
 
  -s  Prep system / install packages (default: no)
 
@@ -23,13 +24,11 @@ Usage: $(basename $0) [-h] [-u driver-url] [-p http://proxy.to.use:port] [ -T ] 
  -u  URL to fetch, known to work:
 
         https://sourceforge.net/projects/e1000/files/i40e%20stable/2.4.6/i40e-2.4.6.tar.gz
-        https://sourceforge.net/projects/e1000/files/i40e%20stable/2.10.6/i40e-2.10.6.tar.gz
+        https://sourceforge.net/projects/e1000/files/i40e%20stable/2.4.10/i40e-2.4.10.tar.gz
 
  -p  proxy string to use (sets both http_proxy and https_proxy)
 
  -T  don't use a temporary directory (default: do use a temp directory)
-
- -d  debug (be a little verbose)
 EOF
 exit 1
 }
@@ -54,9 +53,6 @@ while getopts ":dThp:su:" opt; do
 	    export http_proxy=$OPTARG
 	    export https_proxy=$OPTARG
 	    ;;
-	d )
-	    DEBUG=1
-	    ;;
 	\?)
 	    echo "Invalid: $OPTARG" 1>&2
 	    ERR=1
@@ -71,56 +67,63 @@ done
 
 [ $ERR -ne 0 ] && exit 1
 
-if [ $DEBUG -ne 0 ] ; then
-    echo "URL:   $DRVURL"
-    echo "PROXY: ${https_proxy:-(not set)}"
-fi
+echo "URL:   $DRVURL"
+echo "PROXY: ${https_proxy:-(not set)}"
 
 if [ $PREP -ne 0 ] ; then
     echo "Prepping system"
     apt-get install -y wget build-essential dkms
 fi
 
-if ! dkms status -k $(uname -r) | grep -q installed ; then
-
-    if [ $TEMPDIR -ne 0 ] ; then
+if [ $TEMPDIR -ne 0 ] ; then
 	tmpdir=$(mktemp -d /tmp/i40-install.XXXXXX)
 	function cleanup {
 	    rm -rf "$tmpdir"
 	}
 	trap cleanup EXIT
 	cd $tmpdir
-    fi
+fi
 
-    curl -L --silent $DRVURL | tar -xz
-    # dkms not installed but old dir in the way, rename
-    [ -e /usr/src/i40e-2.4.6 ] && mv -v /usr/src/i40e-2.4.6 /usr/src/i40e-2.4.6.$RANDOM.$RANDOM
-    mv -T i40e-2.4.6/src/ /usr/src/i40e-2.4.6/
-    cat <<EOF > /usr/src/i40e-2.4.6/dkms.conf
+curl -L --silent $DRVURL | tar -xz
+
+# base dir (name)
+bdir=$(ls|grep i40e)
+if [ "$(echo $bdir | wc -w)" -ne 1 ] ; then
+	echo "Unable to determine correct module directory, I see $bdir" 2>&1
+	exit 1
+fi
+
+# extract version
+DRVVER=$(echo $bdir | cut -c6-)
+
+# target dir
+tdir="/usr/src/${bdir}"
+
+echo "VERSION: $DRVVER"
+echo "TARGET:  $tdir"
+
+# dkms not installed but old dir in the way, rename
+[ -e "${tdir}" ] && mv -v "${tdir}" "${tdir}.$RANDOM.$RANDOM"
+mv -T "${bdir}/src" "${tdir}"
+cat <<EOF > "${tdir}/dkms.conf"
 PACKAGE_NAME="i40e"
-PACKAGE_VERSION="2.4.6"
+PACKAGE_VERSION="${DRVVER}"
 BUILT_MODULE_NAME[0]="i40e"
-DEST_MODULE_LOCATION[0]="/kernel/drivers/net/ethernet/intel/i40e/"
+#DEST_MODULE_LOCATION[0]="/kernel/drivers/net/ethernet/intel/i40e/"
+DEST_MODULE_LOCATION[0]="/updates/"
 REMAKE_INITRD="yes"
 AUTOINSTALL="yes"
-#CLEAN="make clean"
+#CLEAN="make -C src/ clean"
+#MAKE="make -C src/ "
 EOF
-    dkms add -m i40e -v 2.4.6
-    # install for all kernels ('dkms autoinstall' won't do this)
-    for kver in $(ls -1 /lib/modules/) ; do dkms autoinstall -k $kver ; done
-fi
-
-# make sure modprobe is going to load the 'right' version
-wver=$(modinfo i40e | grep ^version | awk '{print $2}')
-if [ "$wver" != "2.4.6" ] ; then
-    echo "ERROR: module version to be loaded is $wver"
-    exit 2
-fi
+dkms add -m i40e -v "${DRVVER}"
+# install for other kernels ('dkms autoinstall' won't do this)
+for kver in $(ls /lib/modules/) ; do dkms autoinstall -k $kver ; done
 
 # make sure we have the right module loaded
 if [ -e /sys/module/i40e/version ] ; then
     lver="$(cat /sys/module/i40e/version)"
-    if [ "$lver" != "2.4.6" ] ; then
+    if [ "$lver" != "${DRVVER}" ] ; then
 	echo "Wrong module version loaded $lver, removing." >&2
 	rmmod i40e
     fi
